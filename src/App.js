10c20f0052bd19478943f2bef8ea670e750790d3
hunterstokes <hunterstokes@me.com>
2018-07-12 11:06:29 -0700

    fixing our file pathing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
+
+import Cats from './pages/Cats'
+import NewCat from './pages/NewCat'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <Router>
+                    <Switch>
+                        <Route exact path="/cats" component={Cats} />
+                        <Route exact path="/" component={NewCat} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/pages/Cats.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Cats extends Component {
+    render() {
+        return (
+            <h4> something </h4>
+        )
+    }
+}
+export default Cats;
